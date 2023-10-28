98750da2641a06440f11cdf7585a83e4f5efdd5a
brseghese <brseghese@gmail.com>
2023-10-28 10:46:43 -0300

    project unsplash - query

--- 07-unsplash-images/src/Gallery.jsx
+++ 07-unsplash-images/src/Gallery.jsx
@@ -1,4 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const url =
+  'https://api.unsplash.com/search/photos?client_id=urpcwiIOIxgH12OcJ69dpcjvs-TJIYLcXJJKiGYV0Mg&&query=office';
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+  console.log(response);
   return <h2>Gallery</h2>;
 };
 
