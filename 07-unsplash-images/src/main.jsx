a6901172f8ecab2c0ad8b697520f2fff0303db93
brseghese <brseghese@gmail.com>
2023-10-28 10:36:00 -0300

    project unsplash - react query

--- 07-unsplash-images/src/main.jsx
+++ 07-unsplash-images/src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { AppProvider } from './context';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AppProvider>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </AppProvider>
 );
