0498b230206aa21b3837c78860af0c6e19e2a127
brseghese <brseghese@gmail.com>
2023-10-28 11:11:57 -0300

    project unsplash images - react query dev tools

--- 07-unsplash-images/src/main.jsx
+++ 07-unsplash-images/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import { AppProvider } from './context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <AppProvider>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </AppProvider>
 );
