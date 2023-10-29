02f20acfda6a16f0982401a8eedb0d608c6e42db
brseghese <brseghese@gmail.com>
2023-10-29 04:28:39 -0300

    project unsplash images - check dark mode preference with javascript

--- 07-unsplash-images/src/context.jsx
+++ 07-unsplash-images/src/context.jsx
@@ -2,16 +2,26 @@ import { createContext, useContext, useState, useEffect } from 'react';
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme:dark)'
+  ).matches;
+  console.log(prefersDarkMode);
+  return prefersDarkMode;
+};
+
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
   const [searchTerm, setSearchTerm] = useState('cat');
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
-    const body = document.querySelector('body');
-    body.classList.toggle('dark-theme', newDarkTheme);
   };
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-theme', isDarkTheme);
+  }, [isDarkTheme]);
 
   return (
     <AppContext.Provider
