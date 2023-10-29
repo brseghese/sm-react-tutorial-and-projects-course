7b13e569cb83a1a28bf099fc1a4e7a0ea2c47ef2
brseghese <brseghese@gmail.com>
2023-10-29 05:02:19 -0300

    project unsplash images - dark mode preference in localstorage

--- 07-unsplash-images/src/context.jsx
+++ 07-unsplash-images/src/context.jsx
@@ -6,8 +6,9 @@ const getInitialDarkMode = () => {
   const prefersDarkMode = window.matchMedia(
     '(prefers-color-scheme:dark)'
   ).matches;
-  console.log(prefersDarkMode);
-  return prefersDarkMode;
+
+  const storedDarkMode = localStorage.getItem('darkTheme');
+  return storedDarkMode === null ? prefersDarkMode : storedDarkMode === 'true';
 };
 
 export const AppProvider = ({ children }) => {
@@ -17,6 +18,7 @@ export const AppProvider = ({ children }) => {
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
   };
 
   useEffect(() => {
