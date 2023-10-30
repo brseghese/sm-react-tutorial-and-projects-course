9916b224e9bce2f8239e5be76973f8817c338bb7
brseghese <brseghese@gmail.com>
2023-10-30 05:28:52 -0300

    project mixmaster - link component

--- 09-mixmaster/src/pages/About.jsx
+++ 09-mixmaster/src/pages/About.jsx
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div>
+      <h1>About</h1>
+      <Link to='/'>Home Page</Link>
+    </div>
+  );
 };
 
 export default About;
--- 09-mixmaster/src/pages/HomeLayout.jsx
+++ 09-mixmaster/src/pages/HomeLayout.jsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const HomeLayout = () => {
-  return <h1>HomeLayout</h1>;
+  return (
+    <div>
+      <h1>HomeLayout</h1>
+      <Link to='/about'>About Page </Link>
+      <a href='https://brseghese.github.io/#home'>brseghese</a>
+    </div>
+  );
 };
 
 export default HomeLayout;
