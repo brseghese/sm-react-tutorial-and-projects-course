b80e114a1f115d5549929d3cd154833a29ea9329
brseghese <brseghese@gmail.com>
2023-10-30 14:09:08 -0300

    project mixmaster - navbar styled components

--- 09-mixmaster/src/components/Navbar.jsx
+++ 09-mixmaster/src/components/Navbar.jsx
@@ -1,23 +1,24 @@
 import { NavLink } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/Navbar';
 
 const Navbar = () => {
   return (
-    <nav>
+    <Wrapper>
       <div className='nav-center'>
         <span className='logo'>MixMaster</span>
+        <div className='nav-links'>
+          <NavLink to='/' className='nav-link'>
+            Home
+          </NavLink>
+          <NavLink to='/about' className='nav-link'>
+            About
+          </NavLink>
+          <NavLink to='/newsletter' className='nav-link'>
+            Newsletter
+          </NavLink>
+        </div>
       </div>
-      <div className='nav-links'>
-        <NavLink to='/' className='nav-link'>
-          Home
-        </NavLink>
-        <NavLink to='/about' className='nav-link'>
-          About
-        </NavLink>
-        <NavLink to='/newsletter' className='nav-link'>
-          Newsletter
-        </NavLink>
-      </div>
-    </nav>
+    </Wrapper>
   );
 };
 
