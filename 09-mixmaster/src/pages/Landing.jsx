7157154814458e4e8d713f4eca1ea9aa09a58833
brseghese <brseghese@gmail.com>
2023-10-30 16:17:59 -0300

    project mixmaster - fetch drinks

--- 09-mixmaster/src/pages/Landing.jsx
+++ 09-mixmaster/src/pages/Landing.jsx
@@ -1,12 +1,18 @@
 import { useLoaderData } from 'react-router-dom';
+import axios from 'axios';
+
+const cocktailSearchUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 export const loader = async () => {
-  return 'something';
+  const searchTerm = '';
+  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  return { drinks: response.data.drinks, searchTerm };
 };
 
 const Landing = () => {
-  const data = useLoaderData();
-  console.log(data);
+  const { drinks, searchTerm } = useLoaderData();
+  console.log(drinks);
   return <h1>Landing</h1>;
 };
 
