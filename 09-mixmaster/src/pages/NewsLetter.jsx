6a53e56192d995bfb19432d268b7518204011270
brseghese <brseghese@gmail.com>
2023-10-31 19:25:52 -0300

    project mixmaster - HTML form submission

--- 09-mixmaster/src/pages/NewsLetter.jsx
+++ 09-mixmaster/src/pages/NewsLetter.jsx
@@ -1,8 +1,57 @@
 const NewsLetter = () => {
   return (
-    <div>
-      <h1>NewsLetter</h1>
-    </div>
+    <form className='form' method='POST'>
+      <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        our newsletter
+      </h4>
+
+      <div className='form-row'>
+        <label htmlFor='name' className='form-label'>
+          name
+        </label>
+        <input
+          type='text'
+          className='form-input'
+          name='name'
+          id='name'
+          defaultValue='john'
+        />
+      </div>
+
+      <div className='form-row'>
+        <label htmlFor='lastName' className='form-label'>
+          last name
+        </label>
+        <input
+          type='text'
+          className='form-input'
+          name='lastName'
+          id='lastName'
+          defaultValue='smith'
+        />
+      </div>
+
+      <div className='form-row'>
+        <label htmlFor='email' className='form-label'>
+          email
+        </label>
+        <input
+          type='email'
+          className='form-input'
+          name='email'
+          id='email'
+          defaultValue='test@test.com'
+        />
+      </div>
+
+      <button
+        type='submit'
+        className='btn btn-block'
+        style={{ marginTop: '0.5rem' }}
+      >
+        submit
+      </button>
+    </form>
   );
 };
 
