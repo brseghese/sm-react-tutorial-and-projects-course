837ec3bdf888438a9c3511854ee15d894b70e775
brseghese <brseghese@gmail.com>
2023-10-31 13:47:00 -0300

    project mixmaster - single cocktail render

--- 09-mixmaster/src/pages/Cocktail.jsx
+++ 09-mixmaster/src/pages/Cocktail.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData, Link } from 'react-router-dom';
+import { useLoaderData, Link, Navigate } from 'react-router-dom';
 import axios from 'axios';
 import Wrapper from '../assets/wrappers/CocktailPage';
 
@@ -13,10 +13,65 @@ export const loader = async ({ params }) => {
 
 const Cocktail = () => {
   const { id, data } = useLoaderData();
+
+  if (!data) return <Navigate to='/' />;
+
+  const singleDrink = data.drinks[0];
+
+  const {
+    strDrink: name,
+    strDrinkThumb: image,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions,
+  } = singleDrink;
+
+  const validIngredients = Object.keys(singleDrink)
+    .filter(
+      (key) => key.startsWith('strIngredient') && singleDrink[key] !== null
+    )
+    .map((key) => singleDrink[key]);
+
   return (
-    <div>
-      <h1>Cocktail</h1>
-    </div>
+    <Wrapper>
+      <header>
+        <Link to='/' className='btn'>
+          back home
+        </Link>
+        <h3>{name}</h3>
+      </header>
+      <div className='drink'>
+        <img src={image} alt={name} className='img'></img>
+        <div className='drink-info'>
+          <p>
+            <span className='drink-data'>name :</span> {name}
+          </p>
+          <p>
+            <span className='drink-data'>category :</span> {category}
+          </p>
+          <p>
+            <span className='drink-data'>info :</span> {info}
+          </p>
+          <p>
+            <span className='drink-data'>glass :</span> {glass}
+          </p>
+          <p>
+            <span className='drink-data'>ingredients :</span>
+            {validIngredients.map((item, index) => {
+              return (
+                <span className='ing' key={item}>
+                  {item} {index < validIngredients.length - 1 ? ',' : ''}
+                </span>
+              );
+            })}
+          </p>
+          <p>
+            <span className='drink-data'>instructons :</span> {instructions}
+          </p>
+        </div>
+      </div>
+    </Wrapper>
   );
 };
 
