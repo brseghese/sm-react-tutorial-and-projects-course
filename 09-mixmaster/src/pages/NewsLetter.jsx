956d610e40a097610d4421dce2409acfe0a2be16
brseghese <brseghese@gmail.com>
2023-10-31 19:59:32 -0300

    project mixmaster - submit form

--- 09-mixmaster/src/pages/NewsLetter.jsx
+++ 09-mixmaster/src/pages/NewsLetter.jsx
@@ -1,10 +1,17 @@
-import { Form } from 'react-router-dom';
+import { Form, redirect } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const newsletterUrl = 'https://www.course-api.com/cocktails-newsletter';
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  console.log(data);
-  return null;
+
+  const response = await axios.post(newsletterUrl, data);
+  console.log(response);
+  toast.success(response.data.msg);
+  return redirect('/');
 };
 
 const NewsLetter = () => {
