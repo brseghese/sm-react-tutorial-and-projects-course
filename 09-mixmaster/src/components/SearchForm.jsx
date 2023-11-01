06d820598d0cdda4192b3259ce9924dfa24b827e
brseghese <brseghese@gmail.com>
2023-11-01 05:22:12 -0300

    project mixmaster - searchForm structure

--- 09-mixmaster/src/components/SearchForm.jsx
+++ 09-mixmaster/src/components/SearchForm.jsx
@@ -1,5 +1,23 @@
+import { Form, useNavigation } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/SearchForm';
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+  return (
+    <Wrapper>
+      <Form className='form'>
+        <input
+          type='search'
+          name='search'
+          className='form-input'
+          defaultValue='vodka'
+        />
+        <button type='submit' className='btn' disabled={isSubmitting}>
+          {isSubmitting ? 'searching...' : 'search'}
+        </button>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default SearchForm;
--- 09-mixmaster/src/pages/Landing.jsx
+++ 09-mixmaster/src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from 'react-router-dom';
 import axios from 'axios';
 import CocktailList from '../components/CocktailList';
+import SearchForm from '../components/SearchForm';
 
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
@@ -16,6 +17,7 @@ const Landing = () => {
 
   return (
     <>
+      <SearchForm />
       <CocktailList drinks={drinks} />
     </>
   );
