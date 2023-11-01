fce59f08b6c3458ecd8447bdd73aac9ad24c5f5b
brseghese <brseghese@gmail.com>
2023-11-01 07:39:07 -0300

    project mixmaster - react query landing page setup

--- 09-mixmaster/src/pages/Landing.jsx
+++ 09-mixmaster/src/pages/Landing.jsx
@@ -2,20 +2,32 @@ import { useLoaderData } from 'react-router-dom';
 import axios from 'axios';
 import CocktailList from '../components/CocktailList';
 import SearchForm from '../components/SearchForm';
+import { useQuery } from '@tanstack/react-query';
 
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+const searchCocktailsQuery = (searchTerm) => {
+  return {
+    queryKey: ['search', searchTerm || 'all'],
+    queryFn: async () => {
+      const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+      return response.data.drinks;
+    },
+  };
+};
 
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
 
   const searchTerm = url.searchParams.get('search') || '';
-  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-  return { drinks: response.data.drinks, searchTerm };
+  // const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  return { searchTerm };
 };
 
 const Landing = () => {
-  const { drinks, searchTerm } = useLoaderData();
+  const { searchTerm } = useLoaderData();
+  const { data: drinks } = useQuery(searchCocktailsQuery(searchTerm));
 
   return (
     <>
