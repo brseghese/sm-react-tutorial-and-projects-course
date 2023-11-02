8fdc0113dc299fb281ede7af5c2288ad4457c7d3
brseghese <brseghese@gmail.com>
2023-11-02 18:20:16 -0300

    tailwind portfolio - navbar

--- 10-tailwind-portfolio/src/App.jsx
+++ 10-tailwind-portfolio/src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from './components/Navbar';
+
 const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default App;
