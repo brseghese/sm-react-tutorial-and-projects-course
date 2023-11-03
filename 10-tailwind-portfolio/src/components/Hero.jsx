c9d194b9cd5c547a2f5fbfec422cd734dc2a0226
brseghese <brseghese@gmail.com>
2023-11-03 03:26:57 -0300

    tailwind portfolio - custom class

--- 10-tailwind-portfolio/src/components/Hero.jsx
+++ 10-tailwind-portfolio/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
 const Hero = () => {
   return (
     <section className='bg-emerald-100 py-24  '>
-      <div className=' mx-auto max-w-7xl  px-8  grid  md:grid-cols-2 items-center gap-8'>
+      <div className=' align-element  px-8  grid  md:grid-cols-2 items-center gap-8'>
         <article>
           <h1 className='text-7xl font-bold tracking-wide'>Bruno Seghese</h1>
           <p className='mt-4 text-3xl text-slate-700 capitalize tracking-wide'>
@@ -15,13 +15,13 @@ const Hero = () => {
           </p>
           <div className='flex gap-x-4 mt-4'>
             <a href='#'>
-              <FaGithubSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+              <FaGithubSquare className='h-8 w-8 text-slate-500  hover:text-emerald-600 duration-300' />
             </a>
             <a href='#'>
-              <FaLinkedin className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+              <FaLinkedin className='h-8 w-8 text-slate-500  hover:text-emerald-600 duration-300' />
             </a>
             <a href='#'>
-              <FaTwitterSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+              <FaTwitterSquare className='h-8 w-8 text-slate-500  hover:text-emerald-600 duration-300' />
             </a>
           </div>
         </article>
