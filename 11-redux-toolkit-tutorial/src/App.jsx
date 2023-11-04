6629a290cb9fbc213e466071133b80fe6a4bdff0
brseghese <brseghese@gmail.com>
2023-11-04 15:38:04 -0300

    redux toolkit - modal

--- 11-redux-toolkit-tutorial/src/App.jsx
+++ 11-redux-toolkit-tutorial/src/App.jsx
@@ -3,6 +3,7 @@ import CartContainer from './components/CartContainer';
 import { useDispatch, useSelector } from 'react-redux';
 import { calculateTotals } from './features/cart/cartSlice';
 import { useEffect } from 'react';
+import Modal from './components/Modal';
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart);
@@ -14,6 +15,7 @@ function App() {
 
   return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
