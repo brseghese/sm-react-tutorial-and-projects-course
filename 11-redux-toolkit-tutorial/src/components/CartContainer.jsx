9047a9c10d0b4d3d9c803c6a5d4acdb169333910
brseghese <brseghese@gmail.com>
2023-11-04 14:02:43 -0300

    redux toolkit - reducers - clear cart

--- 11-redux-toolkit-tutorial/src/components/CartContainer.jsx
+++ 11-redux-toolkit-tutorial/src/components/CartContainer.jsx
@@ -1,7 +1,10 @@
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from '../features/cart/cartSlice';
 
 const CartContainer = () => {
+  const dispatch = useDispatch();
+
   const { cartItems, total, amount } = useSelector((state) => state.cart);
 
   if (amount < 1) {
@@ -35,7 +38,9 @@ const CartContainer = () => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button className='btn clear-btn'>clear cart</button>
+        <button className='btn clear-btn' onClick={() => dispatch(clearCart())}>
+          clear cart
+        </button>
       </footer>
     </section>
   );
--- 11-redux-toolkit-tutorial/src/features/cart/cartSlice.js
+++ 11-redux-toolkit-tutorial/src/features/cart/cartSlice.js
@@ -11,6 +11,15 @@ const initialState = {
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+  },
 });
+
+// console.log(cartSlice);
+
+export const { clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
