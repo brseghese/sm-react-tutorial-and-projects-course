46b2c1a76c6ae766e19d2b1fa3f65542d17d767f
brseghese <brseghese@gmail.com>
2023-11-05 13:29:54 -0300

    project comfy-store - navbar structure

--- 12-comfy-store/src/components/index.js
+++ 12-comfy-store/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as FormInput } from './FormInput';
 export { default as SubmitBtn } from './SubmitBtn';
 export { default as Header } from './Header';
+export { default as Navbar } from './Navbar';
--- 12-comfy-store/src/pages/HomeLayout.jsx
+++ 12-comfy-store/src/pages/HomeLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from '../components';
+import { Header, Navbar } from '../components';
 
 const HomeLayout = () => {
   return (
     <>
       <Header />
+      <Navbar />
       <section className='align-element py-8'>
         <Outlet />
       </section>
