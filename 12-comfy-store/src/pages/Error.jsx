96bbbc645653a72093bdfef5066a9ee00103d9f8
brseghese <brseghese@gmail.com>
2023-11-05 03:37:31 -0300

    project comfy-store - error page

--- 12-comfy-store/src/pages/Error.jsx
+++ 12-comfy-store/src/pages/Error.jsx
@@ -1,5 +1,33 @@
+import { useRouteError, Link } from 'react-router-dom';
+
 const Error = () => {
-  return <h1 className='text-4xl'>Error</h1>;
+  const error = useRouteError();
+  console.log(error);
+  if (error.status === 404)
+    return (
+      <main className='grid min-h-[100vh] place-items-center px-8 '>
+        <div className='text-center'>
+          <p className='text-9xl font-semibold text-primary'>404</p>
+          <h1 className='mt-4 text-3xl font-bold tracking-tight sm:text-5xl'>
+            Page not found
+          </h1>
+          <p className='mt-6 text-lg leading-7 '>
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <div className='mt-10 '>
+            <Link to='/' className='btn btn-secondary'>
+              Go back home
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
+
+  return (
+    <main className='grid min-h-[100vh] place-items-center px-8 '>
+      <h4 className='text-center font-bold text-4xl'>there was an error... </h4>
+    </main>
+  );
 };
 
 export default Error;
