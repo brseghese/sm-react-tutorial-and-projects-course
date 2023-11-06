b1cc422f73a2ee48f202014e463dfdfd59763b16
brseghese <brseghese@gmail.com>
2023-11-06 17:27:44 -0300

    project comfy-store - hero component

--- 12-comfy-store/src/components/index.js
+++ 12-comfy-store/src/components/index.js
@@ -2,3 +2,4 @@ export { default as FormInput } from './FormInput';
 export { default as SubmitBtn } from './SubmitBtn';
 export { default as Header } from './Header';
 export { default as Navbar } from './Navbar';
+export { default as Hero } from './Hero';
--- 12-comfy-store/src/pages/Landing.jsx
+++ 12-comfy-store/src/pages/Landing.jsx
@@ -1,5 +1,11 @@
+import { Hero } from '../components';
+
 const Landing = () => {
-  return <h1 className='text-4xl'>Landing</h1>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default Landing;
