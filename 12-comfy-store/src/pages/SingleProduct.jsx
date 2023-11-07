e5150f71cacdd04fca039c8f4468253548f67245
brseghese <brseghese@gmail.com>
2023-11-07 10:58:26 -0300

    project comfy-store - generate amount inputs

--- 12-comfy-store/src/pages/SingleProduct.jsx
+++ 12-comfy-store/src/pages/SingleProduct.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData } from 'react-router-dom';
-import { formatPrice, customFetch } from '../utils';
+import { formatPrice, customFetch, generateAmountOptions } from '../utils';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
@@ -86,9 +86,7 @@ const SingleProduct = () => {
               value={amount}
               onChange={handleAmount}
             >
-              <option value={1}>1</option>
-              <option value={2}>2</option>
-              <option value={3}>3</option>
+              {generateAmountOptions(10)}
             </select>
           </div>
 
