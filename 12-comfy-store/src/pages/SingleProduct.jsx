c167db99dbcd79e40a5a68ccd8bddfa045268312
brseghese <brseghese@gmail.com>
2023-11-07 05:49:05 -0300

    project comfy-store - render single product

--- 12-comfy-store/src/pages/SingleProduct.jsx
+++ 12-comfy-store/src/pages/SingleProduct.jsx
@@ -14,7 +14,40 @@ const SingleProduct = () => {
     product.attributes;
   const dollarsAmount = formatPrice(price);
 
-  return <h1 className='text-4xl'>SingleProduct</h1>;
+  return (
+    <section>
+      <div className='text-md breadcrumbs'>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/products'>Products</Link>
+          </li>
+        </ul>
+      </div>
+      {/* PRODUCT */}
+      <div className='mt-6 grid gap-y-8 lg:grid-cols-2  lg:gap-x-16'>
+        {/* IMAGE */}
+        <img
+          src={image}
+          alt={title}
+          className='w-96 h-96 object-cover rounded-lg lg:w-full  '
+        />
+        {/* PRODUCT INFO */}
+        <div>
+          <h1 className='capitalize text-3xl font-bold'>{title}</h1>
+          <h4 className='text-xl text-neutral-content font-bold mt-2'>
+            {company}
+          </h4>
+
+          <p className='mt-3 text-xl'>{dollarsAmount}</p>
+
+          <p className='mt-6 leading-8'>{description}</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SingleProduct;
