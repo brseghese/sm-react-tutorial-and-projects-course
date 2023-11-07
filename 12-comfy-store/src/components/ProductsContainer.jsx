c0fdcb32e43d47eb8156456212b9cca9663d0c77
brseghese <brseghese@gmail.com>
2023-11-07 15:42:27 -0300

    project comfy-store - products list

--- 12-comfy-store/src/components/ProductsContainer.jsx
+++ 12-comfy-store/src/components/ProductsContainer.jsx
@@ -1,5 +1,13 @@
+import ProductsGrid from './ProductsGrid';
+import ProductsList from './ProductsList';
+
 const ProductsContainer = () => {
-  return <div>ProductsContainer</div>;
+  return (
+    <>
+      <ProductsList />
+      <ProductsGrid />
+    </>
+  );
 };
 
 export default ProductsContainer;
