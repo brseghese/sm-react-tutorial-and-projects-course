1b11c01a3e7de4d78e24295e1d073220f8f71750
brseghese <brseghese@gmail.com>
2023-11-07 17:05:07 -0300

    project comfy-store - filters - select input

--- 12-comfy-store/src/components/Filters.jsx
+++ 12-comfy-store/src/components/Filters.jsx
@@ -1,7 +1,10 @@
 import { Form, useLoaderData, Link } from 'react-router-dom';
 import FormInput from './FormInput';
+import FormSelect from './FormSelect';
 
 const Filters = () => {
+  const { meta } = useLoaderData();
+
   return (
     <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
       {/* SEARCH */}
@@ -10,6 +13,27 @@ const Filters = () => {
         label='search product'
         name='search'
         size='input-sm'
+      />
+      {/* CATEGORIES */}
+      <FormSelect
+        label='select category'
+        name='category'
+        list={meta.categories}
+        size='select-sm'
+      />
+      {/* COMPANIES */}
+      <FormSelect
+        label='select company'
+        name='company'
+        list={meta.companies}
+        size='select-sm'
+      />
+      {/* ORDER */}
+      <FormSelect
+        label='sort by'
+        name='order'
+        list={['a-z', 'z-a', 'high', 'low']}
+        size='select-sm'
       />
       {/* BUTTONS */}
       <button type='submit' className='btn btn-primary btn-sm '>
