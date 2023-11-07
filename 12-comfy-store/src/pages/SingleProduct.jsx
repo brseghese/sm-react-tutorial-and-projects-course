26b0e940c2e31ff7235a690edc061c5567d0a79b
brseghese <brseghese@gmail.com>
2023-11-07 06:35:48 -0300

    project comfy-store - single product amount

--- 12-comfy-store/src/pages/SingleProduct.jsx
+++ 12-comfy-store/src/pages/SingleProduct.jsx
@@ -10,12 +10,16 @@ export const loader = async ({ params }) => {
 
 const SingleProduct = () => {
   const { product } = useLoaderData();
-
   const { image, title, price, description, colors, company } =
     product.attributes;
   const dollarsAmount = formatPrice(price);
 
   const [productColor, setProductColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  const handleAmount = (e) => {
+    setAmount(parseInt(e.target.value));
+  };
 
   return (
     <section>
@@ -29,6 +33,7 @@ const SingleProduct = () => {
           </li>
         </ul>
       </div>
+
       {/* PRODUCT */}
       <div className='mt-6 grid gap-y-8 lg:grid-cols-2  lg:gap-x-16'>
         {/* IMAGE */}
@@ -37,15 +42,14 @@ const SingleProduct = () => {
           alt={title}
           className='w-96 h-96 object-cover rounded-lg lg:w-full  '
         />
+
         {/* PRODUCT INFO */}
         <div>
           <h1 className='capitalize text-3xl font-bold'>{title}</h1>
           <h4 className='text-xl text-neutral-content font-bold mt-2'>
             {company}
           </h4>
-
           <p className='mt-3 text-xl'>{dollarsAmount}</p>
-
           <p className='mt-6 leading-8'>{description}</p>
 
           {/* COLORS */}
@@ -68,6 +72,34 @@ const SingleProduct = () => {
                 );
               })}
             </div>
+          </div>
+
+          {/* AMOUNT */}
+          <div className='form-control w-full max-w-xs'>
+            <label className='label'>
+              <h4 className='text-md font-medium tracking-wider capitalize'>
+                amount
+              </h4>
+            </label>
+            <select
+              className='select select-secondary select-bordered select-md'
+              value={amount}
+              onChange={handleAmount}
+            >
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+            </select>
+          </div>
+
+          {/* CART BUTTON */}
+          <div className='mt-10 '>
+            <button
+              className='btn btn-secondary btn-md'
+              onClick={() => console.log('add to bag')}
+            >
+              Add to bag
+            </button>
           </div>
         </div>
       </div>
