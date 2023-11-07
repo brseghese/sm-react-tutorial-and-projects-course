d0699b480664595a25e7c7323d9d0329195a02c8
brseghese <brseghese@gmail.com>
2023-11-07 06:21:51 -0300

    project comfy-store - single product colors

--- 12-comfy-store/src/pages/SingleProduct.jsx
+++ 12-comfy-store/src/pages/SingleProduct.jsx
@@ -10,9 +10,12 @@ export const loader = async ({ params }) => {
 
 const SingleProduct = () => {
   const { product } = useLoaderData();
+
   const { image, title, price, description, colors, company } =
     product.attributes;
   const dollarsAmount = formatPrice(price);
+
+  const [productColor, setProductColor] = useState(colors[0]);
 
   return (
     <section>
@@ -44,6 +47,28 @@ const SingleProduct = () => {
           <p className='mt-3 text-xl'>{dollarsAmount}</p>
 
           <p className='mt-6 leading-8'>{description}</p>
+
+          {/* COLORS */}
+          <div className='mt-6'>
+            <h4 className='text-md font-medium tracking-wider capitalize'>
+              colors
+            </h4>
+            <div className='mt-2'>
+              {colors.map((color) => {
+                return (
+                  <button
+                    key={color}
+                    type='button'
+                    className={`badge  w-6 h-6 mr-2  ${
+                      color === productColor && 'border-2 border-secondary'
+                    }`}
+                    style={{ backgroundColor: color }}
+                    onClick={() => setProductColor(color)}
+                  ></button>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </section>
