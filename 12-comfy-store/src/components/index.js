fb7e19b7ec728fdc6b461d74b79244e4a718f410
brseghese <brseghese@gmail.com>
2023-11-07 18:14:53 -0300

    project comfy-store - global loading

--- 12-comfy-store/src/components/index.js
+++ 12-comfy-store/src/components/index.js
@@ -9,3 +9,4 @@ export { default as SectionTitle } from './SectionTitle';
 export { default as Filters } from './Filters';
 export { default as ProductsContainer } from './ProductsContainer';
 export { default as PaginationContainer } from './PaginationContainer';
+export { default as Loading } from './Loading';
--- 12-comfy-store/src/pages/HomeLayout.jsx
+++ 12-comfy-store/src/pages/HomeLayout.jsx
@@ -1,14 +1,21 @@
-import { Outlet } from 'react-router-dom';
-import { Header, Navbar } from '../components';
+import { Outlet, useNavigation } from 'react-router-dom';
+import { Header, Navbar, Loading } from '../components';
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === 'loading';
+
   return (
     <>
       <Header />
       <Navbar />
-      <section className='align-element py-8'>
-        <Outlet />
-      </section>
+      {isPageLoading ? (
+        <Loading />
+      ) : (
+        <section className='align-element py-20'>
+          <Outlet />
+        </section>
+      )}
     </>
   );
 };
