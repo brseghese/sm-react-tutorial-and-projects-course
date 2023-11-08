9e60d2139fb1b5e475c8fff167e4795df9520c16
brseghese <brseghese@gmail.com>
2023-11-08 13:14:05 -0300

    project comfy-store - clear, remove and edit reducers

--- 12-comfy-store/src/features/cart/cartSlice.js
+++ 12-comfy-store/src/features/cart/cartSlice.js
@@ -21,23 +21,38 @@ const cartSlice = createSlice({
     addItem: (state, action) => {
       const { product } = action.payload;
       const item = state.cartItems.find((i) => i.cartID === product.cartID);
-
       if (item) {
         item.amount += product.amount;
       } else {
         state.cartItems.push(product);
       }
-
       state.numItemsInCart += product.amount;
       state.cartTotal += product.price * product.amount;
-
       cartSlice.caseReducers.calculateTotals(state);
-
       toast.success('Item added to cart');
     },
-    clearCart: (state) => {},
-    removeItem: (state, action) => {},
-    editItem: (state, action) => {},
+    clearCart: (state) => {
+      localStorage.setItem('cart', JSON.stringify(defaultState));
+      return defaultState;
+    },
+    removeItem: (state, action) => {
+      const { cartID } = action.payload;
+      const product = state.cartItems.find((i) => i.cartID === cartID);
+      state.cartItems = state.cartItems.filter((i) => i.cartID !== cartID);
+      state.numItemsInCart -= product.amount;
+      state.cartTotal -= product.price * product.amount;
+      cartSlice.caseReducers.calculateTotals(state);
+      toast.error('Item removed from cart');
+    },
+    editItem: (state, action) => {
+      const { cartID, amount } = action.payload;
+      const item = state.cartItems.find((i) => i.cartID === cartID);
+      state.numItemsInCart += amount - item.amount;
+      state.cartTotal += item.price * (amount - item.amount);
+      item.amount = amount;
+      cartSlice.caseReducers.calculateTotals(state);
+      toast.success('Cart updated');
+    },
     calculateTotals: (state) => {
       state.tax = 0.1 * state.cartTotal;
       state.orderTotal = state.cartTotal + state.shipping + state.tax;
