a57e3b56f87a11dc8e796921721deb1984d2702f
brseghese <brseghese@gmail.com>
2023-11-08 12:54:29 -0300

    project comfy-store - refactor and local storage

--- 12-comfy-store/src/features/cart/cartSlice.js
+++ 12-comfy-store/src/features/cart/cartSlice.js
@@ -10,9 +10,13 @@ const defaultState = {
   orderTotal: 0,
 };
 
+const getCartFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem('cart')) || defaultState;
+};
+
 const cartSlice = createSlice({
   name: 'cart',
-  initialState: defaultState,
+  initialState: getCartFromLocalStorage(),
   reducers: {
     addItem: (state, action) => {
       const { product } = action.payload;
@@ -26,15 +30,19 @@ const cartSlice = createSlice({
 
       state.numItemsInCart += product.amount;
       state.cartTotal += product.price * product.amount;
-      state.tax = 0.1 * state.cartTotal;
-      state.orderTotal = state.cartTotal + state.shipping + state.tax;
 
-      localStorage.setItem('cart', JSON.stringify(state));
+      cartSlice.caseReducers.calculateTotals(state);
+
       toast.success('Item added to cart');
     },
     clearCart: (state) => {},
     removeItem: (state, action) => {},
     editItem: (state, action) => {},
+    calculateTotals: (state) => {
+      state.tax = 0.1 * state.cartTotal;
+      state.orderTotal = state.cartTotal + state.shipping + state.tax;
+      localStorage.setItem('cart', JSON.stringify(state));
+    },
   },
 });
 
