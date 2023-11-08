e6243bcb612cd89ca0829ae4ea2722a5d9a0fb8e
brseghese <brseghese@gmail.com>
2023-11-08 05:21:31 -0300

    project comfy-store - pagination setup

--- 12-comfy-store/src/components/PaginationContainer.jsx
+++ 12-comfy-store/src/components/PaginationContainer.jsx
@@ -1,5 +1,53 @@
+import { useLoaderData, useLocation, useNavigate } from 'react-router-dom';
+
 const PaginationContainer = () => {
-  return <div>PaginationContainer</div>;
+  const { meta } = useLoaderData();
+  const { pageCount, page } = meta.pagination;
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    return index + 1;
+  });
+
+  const handlePageChange = (pageNumber) => {
+    console.log(pageNumber);
+  };
+
+  if (pageCount < 2) return null;
+
+  return (
+    <div className='mt-16 flex justify-end'>
+      <div className='join'>
+        <button
+          className='btn btn-xs sm:btn-md join-item'
+          onClick={() => {
+            handlePageChange('prev');
+          }}
+        >
+          Prev
+        </button>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              onClick={() => handlePageChange(pageNumber)}
+              key={pageNumber}
+              className={`btn btn-xs sm:btn-md border-none join-item ${
+                pageNumber === page ? 'bg-base-300 border-base-300' : ''
+              }`}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+        <button
+          className='btn btn-xs sm:btn-md join-item'
+          onClick={() => {
+            handlePageChange('next');
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PaginationContainer;
