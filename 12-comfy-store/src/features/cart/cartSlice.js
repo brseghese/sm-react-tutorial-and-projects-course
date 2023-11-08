e18c61d580cb7fb491fdfce0d32a97f754a876f0
brseghese <brseghese@gmail.com>
2023-11-08 10:35:55 -0300

    project comfy-store - redux toolkit setup

---
+++ 12-comfy-store/src/features/cart/cartSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+
+const defaultState = {
+  cartItems: [],
+  numItemsInCart: 0,
+  cartTotal: 0,
+  shipping: 500,
+  tax: 0,
+  orderTotal: 0,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: defaultState,
+  reducers: {
+    addItem: (state, action) => {
+      console.log(action.payload);
+    },
+    clearCart: (state) => {},
+    removeItem: (state, action) => {},
+    editItem: (state, action) => {},
+  },
+});
+
+export const { addItem, removeItem, editItem, clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
--- 12-comfy-store/src/main.jsx
+++ 12-comfy-store/src/main.jsx
@@ -1,9 +1,16 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
+import { ToastContainer } from 'react-toastify';
+import { store } from './store';
+import { Provider } from 'react-redux';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  <Provider store={store}>
     <App />
-  </>
+    <ToastContainer position='top-center' />
+  </Provider>
 );
