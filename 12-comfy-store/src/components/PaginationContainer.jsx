659e23e80f3951292bf6ebcba72372d9c08a758c
brseghese <brseghese@gmail.com>
2023-11-08 05:46:28 -0300

    project comfy-store - pagination complete

--- 12-comfy-store/src/components/PaginationContainer.jsx
+++ 12-comfy-store/src/components/PaginationContainer.jsx
@@ -7,8 +7,13 @@ const PaginationContainer = () => {
     return index + 1;
   });
 
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+
   const handlePageChange = (pageNumber) => {
-    console.log(pageNumber);
+    const searchParams = new URLSearchParams(search);
+    searchParams.set('page', pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
   };
 
   if (pageCount < 2) return null;
@@ -19,7 +24,9 @@ const PaginationContainer = () => {
         <button
           className='btn btn-xs sm:btn-md join-item'
           onClick={() => {
-            handlePageChange('prev');
+            let prevPage = page - 1;
+            if (prevPage < 1) prevPage = pageCount;
+            handlePageChange(prevPage);
           }}
         >
           Prev
@@ -40,7 +47,9 @@ const PaginationContainer = () => {
         <button
           className='btn btn-xs sm:btn-md join-item'
           onClick={() => {
-            handlePageChange('next');
+            let nextPage = page + 1;
+            if (nextPage > pageCount) nextPage = 1;
+            handlePageChange(nextPage);
           }}
         >
           Next
