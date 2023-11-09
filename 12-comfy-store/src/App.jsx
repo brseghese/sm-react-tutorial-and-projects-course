2db1d3e48baae4c9b78efa569dcba1536a489ac2
brseghese <brseghese@gmail.com>
2023-11-09 11:41:26 -0300

    project comfy-store - register user setup

--- 12-comfy-store/src/App.jsx
+++ 12-comfy-store/src/App.jsx
@@ -22,6 +22,7 @@ import { loader as singleProductLoader } from './pages/SingleProduct';
 import { loader as productsLoader } from './pages/Products';
 
 // Actions
+import { action as registerAction } from './pages/Register';
 
 const router = createBrowserRouter([
   {
@@ -71,6 +72,7 @@ const router = createBrowserRouter([
     path: '/register',
     element: <Register />,
     errorElement: <Error />,
+    action: registerAction,
   },
 ]);
 
--- 12-comfy-store/src/pages/Register.jsx
+++ 12-comfy-store/src/pages/Register.jsx
@@ -1,6 +1,10 @@
 import { FormInput, SubmitBtn } from '../components';
 import { Form, Link } from 'react-router-dom';
 
+export const action = async () => {
+  return null;
+};
+
 const Register = () => {
   return (
     <section className='h-screen grid place-items-center'>
