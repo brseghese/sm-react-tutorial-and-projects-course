e493832ef9880586e9c79ec9b36fb5714801041b
brseghese <brseghese@gmail.com>
2023-11-09 11:53:54 -0300

    project comfy-store - register user complete

--- 12-comfy-store/src/pages/Register.jsx
+++ 12-comfy-store/src/pages/Register.jsx
@@ -1,8 +1,24 @@
 import { FormInput, SubmitBtn } from '../components';
-import { Form, Link } from 'react-router-dom';
+import { Form, redirect, Link } from 'react-router-dom';
 
-export const action = async () => {
-  return null;
+import { customFetch } from '../utils';
+import { toast } from 'react-toastify';
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    const response = await customFetch.post('/auth/local/register', data);
+    toast.success('account created successfully');
+    return redirect('/login');
+  } catch (error) {
+    const errorMessage =
+      error?.response?.data?.error?.message ||
+      'please double check your credentials';
+    toast.error(errorMessage);
+    return null;
+  }
 };
 
 const Register = () => {
