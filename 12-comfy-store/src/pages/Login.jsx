50995704be2ef0512d0d47b55d427304190cfb45
brseghese <brseghese@gmail.com>
2023-11-09 12:57:30 -0300

    project comfy-store - login page action complete

--- 12-comfy-store/src/pages/Login.jsx
+++ 12-comfy-store/src/pages/Login.jsx
@@ -1,10 +1,32 @@
 import { FormInput, SubmitBtn } from '../components';
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, redirect, useNavigate } from 'react-router-dom';
+import { customFetch } from '../utils';
+import { toast } from 'react-toastify';
+import { loginUser } from '../features/user/userSlice';
+import { useDispatch } from 'react-redux';
 
-export const action = (store) => async () => {
-  console.log(store);
-  return null;
-};
+export const action =
+  (store) =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+
+    try {
+      const response = await customFetch.post('/auth/local', data);
+
+      store.dispatch(loginUser(response.data));
+      toast.success('logged in successfully');
+      return redirect('/');
+    } catch (error) {
+      console.log(error);
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        'please double check your credentials';
+
+      toast.error(errorMessage);
+      return null;
+    }
+  };
 
 const Login = () => {
   return (
