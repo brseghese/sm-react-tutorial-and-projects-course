257a65c00854ff0792f87a405951be54fb222100
brseghese <brseghese@gmail.com>
2023-11-10 08:16:31 -0300

    project comfy-store - checkout page setup

---
+++ 12-comfy-store/src/components/CheckoutForm.jsx
@@ -0,0 +1,5 @@
+const CheckoutForm = () => {
+  return <h1 className='text-3xl'>CheckoutForm</h1>;
+};
+
+export default CheckoutForm;
--- 12-comfy-store/src/pages/Checkout.jsx
+++ 12-comfy-store/src/pages/Checkout.jsx
@@ -1,5 +1,19 @@
-const Checkout = () => {
-  return <h1 className='text-4xl'>Checkout</h1>;
-};
+import { useSelector } from 'react-redux';
+import { CheckoutForm, SectionTitle, CartTotals } from '../components';
 
+const Checkout = () => {
+  const cartItems = useSelector((state) => state.cartState.cartItems);
+  if (cartItems.length === 0) {
+    return <SectionTitle text='Your cart is empty' />;
+  }
+  return (
+    <>
+      <SectionTitle text='Place your order' />
+      <div className='mt-8 grid gap-8 md:grid-cols-2 items-start'>
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  );
+};
 export default Checkout;
