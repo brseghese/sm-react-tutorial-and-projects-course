c457c3315cacd26293c7efe6aaf47cdea15cb1e6
brseghese <brseghese@gmail.com>
2023-11-10 09:48:34 -0300

    project comfy-store - checkout page complete

--- 12-comfy-store/src/App.jsx
+++ 12-comfy-store/src/App.jsx
@@ -25,6 +25,7 @@ import { loader as checkoutLoader } from './pages/Checkout';
 // Actions
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
+import { action as checkoutAction } from './components/CheckoutForm';
 
 import { store } from './store';
 
@@ -61,6 +62,7 @@ const router = createBrowserRouter([
         path: 'checkout',
         element: <Checkout />,
         loader: checkoutLoader(store),
+        action: checkoutAction(store),
       },
       {
         path: 'orders',
